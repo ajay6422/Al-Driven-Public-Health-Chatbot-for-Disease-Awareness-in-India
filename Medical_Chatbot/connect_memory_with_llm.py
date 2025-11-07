d395f9a2cd5cec596b0a78d2dfd7f98564c30d0e
Ajay kumar <ajay.12215248@lpu.in>
2025-11-07 20:31:35 +0530

    Add files via upload

---
+++ Medical_Chatbot/connect_memory_with_llm.py
@@ -0,0 +1,69 @@
+import os
+
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
+from langchain.chains import RetrievalQA
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+
+## Uncomment the following files if you're not using pipenv as your virtual environment manager
+#from dotenv import load_dotenv, find_dotenv
+#load_dotenv(find_dotenv())
+
+
+# Step 1: Setup LLM (Mistral with HuggingFace)
+HF_TOKEN=os.environ.get("HF_TOKEN")
+HUGGINGFACE_REPO_ID="mistralai/Mistral-7B-Instruct-v0.3"
+
+from langchain_openai import ChatOpenAI
+
+def load_llm(model_name="lmstudio", base_url="http://localhost:1234/v1", temperature=0.5):
+    model_name = "liquid/lfm2-1.2b"
+
+    llm = ChatOpenAI(
+        model=model_name,
+        openai_api_key="lm-studio",  # dummy key (LM Studio ignores it)
+        openai_api_base=base_url,    # local endpoint
+        temperature=temperature,
+    )
+    return llm
+
+# Step 2: Connect LLM with FAISS and Create chain
+
+CUSTOM_PROMPT_TEMPLATE = """
+Use the pieces of information provided in the context to answer user's question.
+If you dont know the answer, just say that you dont know, dont try to make up an answer. 
+Dont provide anything out of the given context
+
+Context: {context}
+Question: {question}
+
+Start the answer directly. No small talk please.
+"""
+
+def set_custom_prompt(custom_prompt_template):
+    prompt=PromptTemplate(template=custom_prompt_template, input_variables=["context", "question"])
+    return prompt
+
+# Load Database
+DB_FAISS_PATH="vectorstore/db_faiss"
+embedding_model=HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+db=FAISS.load_local(DB_FAISS_PATH, embedding_model, allow_dangerous_deserialization=True)
+
+# Create QA chain
+qa_chain=RetrievalQA.from_chain_type(
+    llm=load_llm(),
+    chain_type="stuff",
+    retriever=db.as_retriever(search_kwargs={'k':3}),
+    return_source_documents=True,
+    chain_type_kwargs={'prompt':set_custom_prompt(CUSTOM_PROMPT_TEMPLATE)}
+)
+import os
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+
+# Now invoke with a single query
+user_query=input("Write Query Here: ")
+response=qa_chain.invoke({'query': user_query})
+print("RESULT: ", response["result"])
+print("SOURCE DOCUMENTS: ", response["source_documents"])
